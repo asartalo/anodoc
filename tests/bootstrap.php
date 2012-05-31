a0412c131fa0a66b7507a40227f017721252f6c6
asartalo <asartalo@projectweb.ph>
2012-06-01 04:10:15 +0800

    Added composer.json and reconfigured project to use composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,2 @@
 <?php
-$src_path = realpath(__DIR__ . '/..') . '/src/';
-require_once realpath(__DIR__ . '/../') .
-    '/vendor/AsarClassLoader/src/Asar/ClassLoader.php';
-$classLoader = new \Asar\ClassLoader('Anodoc\Tests', __DIR__);
-$classLoader->register();
-$classLoader = new \Asar\ClassLoader('Anodoc', $src_path);
-$classLoader->register();
+require_once realpath(__DIR__ . '/..') . '/vendor/autoload.php';
